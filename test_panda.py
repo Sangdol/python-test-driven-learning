0242535f9cde296da05c3697301249ca88da0e56
Sanghyun Lee <hammerha@gmail.com>
2018-09-03 13:52:01 +0200

    fix pandas dependency

--- test_panda.py
+++ test_panda.py
@@ -5,15 +5,15 @@ import pandas as pd
 class PandaTest(unittest.TestCase):
 
     def test_read_csv_and_data_frame(self):
-        df = pd.read_csv('test_panda.csv')
+        df = pd.read_csv('stub/test_panda.csv')
         self.assertEqual(df.size, 4);
         self.assertEqual(df.at[0, 'name'], 'sang')
         self.assertEqual(df.loc[1].at['name'], 'kim')
         self.assertEqual(df.iat[0, 1], 36)
 
     def test_data_frame_merge(self):
-        df1 = pd.read_csv('test_panda.csv')
-        df2 = pd.read_csv('test_panda_join.csv')
+        df1 = pd.read_csv('stub/test_panda.csv')
+        df2 = pd.read_csv('stub/test_panda_join.csv')
 
         merged = pd.merge(df1, df2)
         self.assertEqual(merged.at[0, 'name'], 'sang')
