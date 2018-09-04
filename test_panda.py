b9f8956a18957dc233810c0d63d2f6399ba449ce
Sanghyun Lee <hammerha@gmail.com>
2018-09-04 22:33:20 +0200

    Add pandas series test

--- test_panda.py
+++ test_panda.py
@@ -1,9 +1,40 @@
 import unittest
 
 import pandas as pd
+import numpy as np
 
 
 class PandaTest(unittest.TestCase):
+
+    def assertArrayEqual(self, arr1, arr2):
+        self.assertTrue(np.array_equal(arr1, arr2))
+
+    def test_series(self):
+        self.assertEqual(str(pd.Series([1]).dtype), 'int64')
+        self.assertEqual(str(pd.Series([1, None]).dtype), 'float64')
+        self.assertEqual(str(pd.Series(['a', None]).dtype), 'object')
+
+        # NAN
+        nan = pd.Series([1, None])[1]
+        self.assertEqual(str(nan), 'nan')
+        self.assertNotEqual(nan, np.nan)
+        self.assertTrue(np.isnan(nan))
+
+        # dictionary
+        numbers = {'one': 1, 'two': 2}
+        s = pd.Series(numbers)
+        self.assertArrayEqual(s.index.values, ['one', 'two'])
+        self.assertArrayEqual(s.values, [1, 2])
+
+        s = pd.Series([1, 2], index=['one', 'two'])
+        self.assertArrayEqual(s.index.values, ['one', 'two'])
+        self.assertArrayEqual(s.values, [1, 2])
+
+        s = pd.Series(s, index=['one', 'three'])
+        self.assertEqual(s[0], 1)
+        self.assertEqual(s.index[0], 'one')
+        self.assertTrue(np.isnan(s[1]))
+        self.assertEqual(s.index[1], 'three')
 
     def test_read_csv_and_data_frame(self):
         df = pd.read_csv('stub/test_panda.csv')
