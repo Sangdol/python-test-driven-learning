f5202e7260c0c09f5915d1cc987c2d0570154b7d
Sanghyun Lee <hammerha@gmail.com>
2018-10-25 22:53:50 +0200

    pytestify numpy tests

--- test_numpy.py
+++ test_numpy.py
@@ -1,89 +1,89 @@
-import unittest
 import numpy as np
 import pandas as pd
 import collections
+import pytest
 
 
-class NumpyTest(unittest.TestCase):
-
-    # https://docs.scipy.org/doc/numpy/reference/generated/numpy.array_equal.html
-    def assertArrayEqual(self, arr1, arr2):
-        if not np.array_equal(arr1, arr2):
-            self.fail("{} is not {}".format(arr1, arr2))
-
-    # Cannot get 'day' from datetime64. Use pd.to_datetime('') instead
-    def test_date_comparison(self):
-        self.assertTrue(np.datetime64('2018-01-01') < np.datetime64('2018-01-02'))
-        self.assertTrue(np.datetime64('2018-01-01') == np.datetime64('2018-01-01'))
-
-    # https://stackoverflow.com/questions/41550746/error-using-astype-when-nan-exists-in-a-dataframe
-    def test_nan_type(self):
-        self.assertEqual(type(np.nan).__name__, 'float')
-
-        arr = np.array([np.nan, 0])
-
-        self.assertEqual(str(arr.dtype), 'float64')
-        self.assertArrayEqual(arr.astype(int), [-9223372036854775808, 0])  # ??
-
-        try:
-            """
-            cannot cast to int as nan is float
-            """
-            pd.Series(arr).astype(int)
-            self.fail()
-        except ValueError:
-            pass
-
-    # https://stackoverflow.com/questions/36000993/numpy-isnan-fails-on-an-array-of-floats-from-pandas-dataframe-apply
-    def test_isnan_type_error(self):
-        arr = np.array([np.nan, 0], dtype=object)
-        try:
-            """
-            np.isnan can be applied to NumPy arrays of native dtype (such as np.float64):
-            """
-            np.isnan(arr)
-            self.fail()
-        except TypeError:
-            self.assertArrayEqual(pd.isnull(arr), [True, False])
-            pass
-
-    def test_argwhere(self):
-        arr = np.array([0, 0, np.nan, 1, 1, 1])
-        
-        self.assertArrayEqual(np.argwhere(arr == 0), [[0], [1]])
-        self.assertArrayEqual(np.argwhere(np.isnan(arr)), [[2]])
-
-    # https://stackoverflow.com/questions/28663856/how-to-count-the-occurrence-of-certain-item-in-an-ndarray-in-python
-    def test_counting(self):
-        y = np.array([0, 0, 1, 1, 1])
-        self.assertEqual(np.sum(y), 3)
-        self.assertEqual(np.count_nonzero(y), 3)
-        self.assertEqual(collections.Counter(y), {0: 2, 1: 3})
-        self.assertEqual(len(y[y == 1]), 3)
-        self.assertEqual((y == 1).sum(), 3)
-        self.assertEqual(list(y).count(1), 3)
-
-    # For conditional (ternary) operation
-    # https://docs.scipy.org/doc/numpy/reference/generated/numpy.where.html
-    def test_where(self):
-        x = np.array([1, 2, 3])
-        self.assertArrayEqual(np.where(x >= 2, x, 0), [0, 2, 3])
-
-    def test_transpose(self):
-        self.assertArrayEqual(np.transpose([1, 2]), [1, 2])
-        self.assertArrayEqual(np.transpose([[1, 2]]), [[1], [2]])
-
-    # https://docs.scipy.org/doc/numpy/reference/generated/numpy.arange.html
-    def test_arange(self):
-        self.assertArrayEqual(np.arange(3), [0, 1, 2])
-
-        # start, stop, step
-        self.assertArrayEqual(np.arange(10, 16, 2), [10, 12, 14])
-
-        # https://stackoverflow.com/questions/10580676/comparing-two-numpy-arrays-for-equality-element-wise
-        self.assertArrayEqual(np.arange(3, 5) == [3, 4], [True, True])
-        self.assertTrue((np.arange(3, 5) == [3, 4]).all())
+def assert_array_equal(arr1, arr2):
+    if not np.array_equal(arr1, arr2):
+        raise ValueError("{} is not {}".format(arr1, arr2))
 
 
-if __name__ == '__main__':
-    unittest.main();
+# Cannot get 'day' from datetime64. Use pd.to_datetime('') instead
+def test_date_comparison():
+    assert np.datetime64('2018-01-01') < np.datetime64('2018-01-02')
+    assert np.datetime64('2018-01-01') == np.datetime64('2018-01-01')
+
+
+# https://stackoverflow.com/questions/41550746/error-using-astype-when-nan-exists-in-a-dataframe
+def test_nan_type():
+    assert type(np.nan).__name__ == 'float'
+
+    arr = np.array([np.nan, 0])
+
+    assert str(arr.dtype) == 'float64'
+    assert_array_equal(arr.astype(int), [-9223372036854775808, 0])  # ??
+
+    try:
+        """
+        cannot cast to int as nan is float
+        """
+        pd.Series(arr).astype(int)
+        pytest.fail()
+    except ValueError:
+        pass
+
+
+# https://stackoverflow.com/questions/36000993/numpy-isnan-fails-on-an-array-of-floats-from-pandas-dataframe-apply
+def test_isnan_type_error():
+    arr = np.array([np.nan, 0], dtype=object)
+    try:
+        """
+        np.isnan can be applied to NumPy arrays of native dtype (such as np.float64):
+        """
+        np.isnan(arr)
+        pytest.fail()
+    except TypeError:
+        assert_array_equal(pd.isnull(arr), [True, False])
+
+
+def test_argwhere():
+    arr = np.array([0, 0, np.nan, 1, 1, 1])
+
+    assert_array_equal(np.argwhere(arr == 0), [[0], [1]])
+    assert_array_equal(np.argwhere(np.isnan(arr)), [[2]])
+
+
+# https://stackoverflow.com/questions/28663856/how-to-count-the-occurrence-of-certain-item-in-an-ndarray-in-python
+def test_counting():
+    y = np.array([0, 0, 1, 1, 1])
+    assert np.sum(y) == 3
+    assert np.count_nonzero(y) == 3
+    assert collections.Counter(y) == {0: 2, 1: 3}
+    assert len(y[y == 1]) == 3
+    assert (y == 1).sum() == 3
+    assert list(y).count(1) == 3
+
+
+# For conditional (ternary) operation
+# https://docs.scipy.org/doc/numpy/reference/generated/numpy.where.html
+def test_where():
+    x = np.array([1, 2, 3])
+    assert_array_equal(np.where(x >= 2, x, 0), [0, 2, 3])
+
+
+def test_transpose():
+    assert_array_equal(np.transpose([1, 2]), [1, 2])
+    assert_array_equal(np.transpose([[1, 2]]), [[1], [2]])
+
+
+# https://docs.scipy.org/doc/numpy/reference/generated/numpy.arange.html
+def test_arange():
+    assert_array_equal(np.arange(3), [0, 1, 2])
+
+    # start, stop, step
+    assert_array_equal(np.arange(10, 16, 2), [10, 12, 14])
+
+    # https://stackoverflow.com/questions/10580676/comparing-two-numpy-arrays-for-equality-element-wise
+    assert_array_equal(np.arange(3, 5) == [3, 4], [True, True])
+    assert (np.arange(3, 5) == [3, 4]).all()
