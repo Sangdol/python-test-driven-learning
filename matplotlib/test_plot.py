33b49a7a1af4ab95303a2afc45b94017eb1ec99f
Sanghyun Lee <hammerha@gmail.com>
2018-11-25 21:06:35 +0100

    Add print_png test

--- matplotlib/test_plot.py
+++ matplotlib/test_plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pytest
 
 
 # https://matplotlib.org/api/_as_gen/matplotlib.pyplot.plot.html
@@ -21,3 +22,28 @@ def test_basic_plot():
     assert marker_line.get_data() == ([1], [3])
     assert marker_line.get_color() == '#8c564b'
     assert marker_line.get_marker() == '.'
+
+
+# https://matplotlib.org/api/backend_agg_api.html
+@pytest.mark.skip(reason="Nothing to test. Just a code sample.")
+def test_plot_without_scripting_layer():
+    # First let's set the backend without using mpl.use() from the scripting layer
+    from matplotlib.backends.backend_agg import FigureCanvasAgg
+    from matplotlib.figure import Figure
+
+    # create a new figure
+    fig = Figure()
+
+    # associate fig with the backend
+    canvas = FigureCanvasAgg(fig)
+
+    # add a subplot to the fig
+    ax = fig.add_subplot(111)
+
+    # plot the point (3,2)
+    ax.plot(3, 2, '.')
+
+    # save the figure to test.png
+    # you can see this figure in your Jupyter workspace afterwards by going to
+    # https://hub.coursera-notebooks.org/
+    canvas.print_png('test.png')
