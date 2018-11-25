7333ac92ac3e53bb06fc478e287712fc483833a9
Sanghyun Lee <hammerha@gmail.com>
2018-11-25 12:36:14 +0100

    Rename

--- test_plot.py
+++ test_plot.py
@@ -5,7 +5,7 @@ plt.figure()
 # subplot with 1 row, 2 columns, and current axis is 1st subplot axes
 plt.subplot(1, 2, 1)
 
-linear_data = np.array([1,2,3,4,5,6,7,8])
+linear_data = np.array([1, 2, 3, 4, 5, 6, 7, 8])
 
 plt.plot(linear_data, '-o')
 plt.show()
