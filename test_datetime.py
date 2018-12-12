97d3e62a17acf12b76a9c7d5d85db32077349093
Sanghyun Lee <hammerha@gmail.com>
2018-12-12 16:53:07 +0100

    Datetime formatting tests

--- test_datetime.py
+++ test_datetime.py
@@ -1,5 +1,29 @@
+"""
+https://docs.python.org/3/library/datetime.html
+"""
 import datetime as dt
 import time as tm
+
+
+def test_timezone():
+    d = dt.datetime(2007, 12, 6, 15, 29, 43, 79060, tzinfo=dt.timezone.utc)
+    assert d.isoformat() == '2007-12-06T15:29:43.079060+00:00'
+
+    d = dt.datetime.now(dt.timezone.utc)
+    assert d.strftime('%Z') == 'UTC'
+
+
+# Format codes
+# https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+def test_datetime_format():
+    d = dt.datetime(2007, 12, 6, 15, 29, 43, 79060)
+
+    assert d.strftime('%Y-%m-%d %H:%M:%S') == '2007-12-06 15:29:43'
+    assert d.strftime('%y %B %b') =='07 December Dec'
+
+    # https://en.wikipedia.org/wiki/ISO_8601
+    assert d.isoformat() == '2007-12-06T15:29:43.079060'
+    assert d.isoformat(' ') == '2007-12-06 15:29:43.079060'
 
 
 def test_datetime():
