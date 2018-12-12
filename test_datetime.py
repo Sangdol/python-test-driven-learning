1ae3b6d1d03600454d1477221482699be82ebedb
Sanghyun Lee <hammerha@gmail.com>
2018-12-12 16:15:17 +0100

    Unittest to pytest

--- test_datetime.py
+++ test_datetime.py
@@ -1,22 +1,16 @@
-import unittest
 import datetime as dt
 import time as tm
 
 
-class DatetimeTest(unittest.TestCase):
+def test_datetime():
+    # epoch
+    assert tm.time() > 0
 
-    def test_datetime(self):
-        # epoch
-        self.assertTrue(tm.time() > 0)
+    dtnow = dt.datetime.fromtimestamp(tm.time())
+    assert dtnow.year > 2017
 
-        dtnow = dt.datetime.fromtimestamp(tm.time())
-        self.assertTrue(dtnow.year > 2017)
+    delta = dt.timedelta(days=100)
+    today = dt.date.today()
 
-        delta = dt.timedelta(days=100)
-        today = dt.date.today()
+    assert today > today - delta
 
-        self.assertTrue(today > today - delta)
-
-
-if __name__ == '__main__':
-    unittest.main()
