56ab32e9e9378211a703b39dec702613a9b04f0b
Sanghyun Lee <hammerha@gmail.com>
2019-01-16 18:16:07 +0100

    Add isnumeric() tests

--- test_string.py
+++ test_string.py
@@ -1,3 +1,11 @@
+def test_isnumeric():
+    assert '123'.isnumeric()
+    assert not '-123'.isnumeric()
+    assert not '12.3'.isnumeric()
+    assert not '-12.3'.isnumeric()
+    assert not 'a1'.isnumeric()
+
+
 # https://stackoverflow.com/questions/3437059/does-python-have-a-string-contains-substring-method
 def test_contains():
     # https://docs.python.org/3/reference/expressions.html#membership-test-details
