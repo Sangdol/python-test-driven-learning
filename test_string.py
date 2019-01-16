d4b54bc179bfd7937871ee7514251cc33bd544f7
Sanghyun Lee <hammerha@gmail.com>
2019-01-16 17:16:43 +0100

    Add test_string file

---
+++ test_string.py
@@ -0,0 +1,8 @@
+# https://stackoverflow.com/questions/3437059/does-python-have-a-string-contains-substring-method
+def test_contains():
+    # https://docs.python.org/3/reference/expressions.html#membership-test-details
+    assert 'abc' in 'abcde'
+    assert '123' not in 'abcde'
+
+    assert 'abc'.find('b') == 1
+    assert 'abc'.find('0') == -1
