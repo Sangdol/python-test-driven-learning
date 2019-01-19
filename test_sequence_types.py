48ca5f2a522609b7970a98da2fd795467c83fd95
Sanghyun Lee <hammerha@gmail.com>
2019-01-19 17:59:44 +0100

    Pytestify

--- test_sequence_types.py
+++ test_sequence_types.py
@@ -1,69 +1,63 @@
-import unittest
+def test_tuple():
+    t = (1, 'a', 2, 'b')
+    assert len(t) == 4
+
+    # unpacking
+    a, b = ('a', 'b')
+    assert a == 'a'
 
 
-class SequenceTest(unittest.TestCase):
+def test_list():
+    l = [1, 2, 3]
+    assert len(l) == 3
 
-    def test_tuple(self):
-        t = (1, 'a', 2, 'b')
-        self.assertEqual(len(t), 4)
+    l.append(4)
+    assert len(l) == 4
 
-        # unpacking
-        a, b = ('a', 'b')
-        self.assertEqual(a, 'a')
+    assert [1] + [2] == [1, 2]
+    assert [1] * 3 == [1, 1, 1]
+    assert 1 in [1, 2]
 
-    def test_list(self):
-        l = [1, 2, 3]
-        self.assertEqual(len(l), 3)
+    # slice
+    assert 'hello'[0:2] == 'he'
+    assert 'hello'[-4:-2] == 'el'
+    assert 'hello'[-2:] == 'lo'
+    assert 'hello'[:3] == 'hel'
+    assert 'hello'[3:] == 'lo'
 
-        l.append(4)
-        self.assertEqual(len(l), 4)
-
-        self.assertEqual([1] + [2], [1, 2])
-        self.assertEqual([1] * 3, [1, 1, 1])
-        self.assertTrue(1 in [1, 2])
-
-        # slice
-        self.assertEqual('hello'[0:2], 'he')
-        self.assertEqual('hello'[-4:-2], 'el')
-        self.assertEqual('hello'[-2:], 'lo')
-        self.assertEqual('hello'[:3], 'hel')
-        self.assertEqual('hello'[3:], 'lo')
-
-        # string
-        self.assertTrue('he' in 'hello')
-        self.assertEqual('he' * 3, 'hehehe')
-        self.assertEqual('hello world'.split(' ')[-1], 'world')
-
-    def test_dictionary_functions(self):
-        x = {'hello': 'world'}
-
-        self.assertEqual(x.get('hi', 'default'), 'default')
-
-    def test_dictionary(self):
-        x = {'hello': 'world'}
-        self.assertEqual(x['hello'], 'world')
-
-        x['hallo'] = 'welt'
-        self.assertEqual(x['hallo'], 'welt')
-
-        keys = ''
-        for key in x:
-            keys += key
-
-        self.assertEqual(keys, 'hellohallo')
-
-        values = ''
-        for value in x.values():
-            values += value
-
-        self.assertEqual(values, 'worldwelt')
-
-        key_value = ''
-        for key, value in x.items():
-            key_value += (key + value)
-
-        self.assertEqual(key_value, 'helloworldhallowelt')
+    # string
+    assert 'he' in 'hello'
+    assert 'he' * 3 == 'hehehe'
+    assert 'hello world'.split(' ')[-1] == 'world'
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_dictionary_functions():
+    x = {'hello': 'world'}
+
+    assert x.get('hi', 'default') == 'default'
+
+
+def test_dictionary():
+    x = {'hello': 'world'}
+    assert x['hello'] == 'world'
+
+    x['hallo'] = 'welt'
+    assert x['hallo'] == 'welt'
+
+    keys = ''
+    for key in x:
+        keys += key
+
+    assert keys == 'hellohallo'
+
+    values = ''
+    for value in x.values():
+        values += value
+
+    assert values == 'worldwelt'
+
+    key_value = ''
+    for key, value in x.items():
+        key_value += (key + value)
+
+    assert key_value == 'helloworldhallowelt'
