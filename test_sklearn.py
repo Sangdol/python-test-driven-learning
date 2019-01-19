30f1b77ff3be03f2bb66221c5c6a4c98b365edb0
Sanghyun Lee <hammerha@gmail.com>
2019-01-19 16:10:54 +0100

    Add sklearn datasets tests

--- test_sklearn.py
+++ test_sklearn.py
@@ -1,11 +1,36 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_iris
 
 
 def assert_array_equal(arr1, arr2):
     if not np.array_equal(arr1, arr2):
         raise ValueError("{} is not {}".format(arr1, arr2))
+
+
+# https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_iris.html
+def test_datasets():
+    iris = load_iris()
+    assert type(iris).__module__ == 'sklearn.utils'
+    assert type(iris).__name__ == 'Bunch'
+
+    assert_array_equal(list(iris), ['data', 'target', 'target_names', 'DESCR', 'feature_names', 'filename'])
+    assert iris.data.shape == (150, 4)
+    assert type(iris.data).__name__ == 'ndarray'
+    assert iris.target.shape == (150,)
+    assert type(iris.target).__name__ == 'ndarray'
+
+    assert iris.filename.find('sklearn/datasets/data/iris.csv') > -1  # file location
+
+    assert_array_equal(iris.feature_names,
+                       ['sepal length (cm)', 'sepal width (cm)',
+                        'petal length (cm)', 'petal width (cm)'])
+    assert_array_equal(iris.target_names, ['setosa', 'versicolor', 'virginica'])
+
+    data, target = load_iris(return_X_y=True)
+    assert data.shape == (150, 4)
+    assert target.shape == (150,)
 
 
 # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
