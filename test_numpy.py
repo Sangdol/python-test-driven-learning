21c24081f0181ae0d7d5dce74500d0c29973f272
Sanghyun Lee <hammerha@gmail.com>
2019-02-04 21:05:09 +0100

    Add random seed and randn tests

--- test_numpy.py
+++ test_numpy.py
@@ -14,6 +14,20 @@ def assert_array_not_equal(arr1, arr2):
         raise ValueError("{} is {}".format(arr1, arr2))
 
 
+# https://stackoverflow.com/questions/21494489/what-does-numpy-random-seed0-do
+def test_random_seed():
+    np.random.seed(0)
+    assert np.allclose(np.random.rand(4), [0.5488135 , 0.71518937, 0.60276338, 0.54488318])
+
+    # seed() applies only once
+    assert not np.allclose(np.random.rand(4), [0.5488135 , 0.71518937, 0.60276338, 0.54488318])
+
+
+# standard normal distribution
+def test_randn():
+    assert_array_equal(np.random.randn(2, 3, 4).shape, (2, 3, 4))
+
+
 def test_amax():
     a = [[0, 1], [2, 3]]
 
