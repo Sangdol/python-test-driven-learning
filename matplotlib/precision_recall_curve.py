9abfc5152b5ab81fa6305c1a92ebe4afee99ff09
Sanghyun Lee <hammerha@gmail.com>
2019-02-15 20:12:35 +0100

    Add precision recall curve coursera example

--- matplotlib/precision_recall_curve.py
+++ matplotlib/precision_recall_curve.py
@@ -28,18 +28,42 @@ classifier = svm.LinearSVC(random_state=random_state)
 classifier.fit(X_train, y_train)
 y_score = classifier.decision_function(X_test)
 
-precision, recall, _ = precision_recall_curve(y_test, y_score)
+precision, recall, thresholds = precision_recall_curve(y_test, y_score)
 
-# In matplotlib < 1.5, plt.fill_between does not have a 'step' argument
-step_kwargs = ({'step': 'post'}
-               if 'step' in signature(plt.fill_between).parameters
-               else {})
-plt.step(recall, precision, color='b', alpha=0.2,
-         where='post')
-plt.fill_between(recall, precision, alpha=0.2, color='b', **step_kwargs)
+plt.figure()
 
-plt.xlabel('Recall')
-plt.ylabel('Precision')
-plt.ylim([0.0, 1.05])
-plt.xlim([0.0, 1.0])
-plt.show()
+
+def site_example():
+    # In matplotlib < 1.5, plt.fill_between does not have a 'step' argument
+    step_kwargs = ({'step': 'post'}
+                   if 'step' in signature(plt.fill_between).parameters
+                   else {})
+    plt.step(recall, precision, color='b', alpha=0.2, where='post')
+    plt.fill_between(recall, precision, alpha=0.2, color='b', **step_kwargs)
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.show()
+
+
+def coursera_example():
+    # closest_zero point: default prediction value
+    # https://www.coursera.org/learn/python-machine-learning/discussions/weeks/3/threads/p07BAwQoEemfNRJVokW6aA
+    # https://stats.stackexchange.com/questions/329857/what-is-the-difference-between-decision-function-predict-proba-and-predict-fun/329870#329870
+    closest_zero = np.argmin(np.abs(thresholds))
+    closest_zero_p = precision[closest_zero]
+    closest_zero_r = recall[closest_zero]
+
+    plt.xlim([0.0, 1.01])
+    plt.ylim([0.0, 1.01])
+    plt.plot(precision, recall, label='Precision-Recall Curve')
+    plt.plot(closest_zero_p, closest_zero_r, 'o', markersize = 12, fillstyle = 'none', c='r', mew=3)
+    plt.xlabel('Precision', fontsize=16)
+    plt.ylabel('Recall', fontsize=16)
+    plt.axes().set_aspect('equal')
+    plt.show()
+
+
+coursera_example()
