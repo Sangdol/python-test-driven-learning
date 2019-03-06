0cb4876d3636fc35a3b10a8e0ff89f96e1ade97f
Sanghyun Lee <hammerha@gmail.com>
2019-03-06 13:57:55 +0100

    Add series to json test

--- test_json.py
+++ test_json.py
@@ -1,9 +1,10 @@
 import json
+import pandas as pd
 
 
-def test_export_and_import_json():
+def test_export_and_import_dict():
     d = {'a': 1, 'b': 2, 'c': '3'}
-    FILE_PATH = 'stub/test_json.json'
+    FILE_PATH = 'stub/test_json_dict.json'
 
     # write
     with open(FILE_PATH, 'w') as jf:
@@ -13,7 +14,23 @@ def test_export_and_import_json():
     with open(FILE_PATH) as jf:
         dd = json.loads(jf.read())
 
-        print(dd)
         assert dd['a'] == 1
         assert dd['b'] == 2
         assert dd['c'] == '3'
+
+
+def test_export_and_import_series():
+    s = pd.Series([1, 2], index=['a', 'b'])
+
+    FILE_PATH = 'stub/test_json_dataframe.json'
+
+    # write
+    with open(FILE_PATH, 'w') as jf:
+        json.dump(s.to_dict(), jf)
+
+    # read
+    with open(FILE_PATH) as jf:
+        dd = json.loads(jf.read())
+
+        assert dd['a'] == 1
+        assert dd['b'] == 2
