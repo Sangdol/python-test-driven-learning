e1c1eddd4f32f44768d2033ea0044cd6916399e5
Sanghyun Lee <hammerha@gmail.com>
2019-04-01 17:39:10 +0200

    Add np.newaxis tests

--- test_numpy.py
+++ test_numpy.py
@@ -14,6 +14,17 @@ def assert_array_not_equal(arr1, arr2):
         raise ValueError("{} is {}".format(arr1, arr2))
 
 
+# https://docs.scipy.org/doc/numpy/reference/constants.html#numpy.newaxis
+def test_newaxis():
+    assert np.newaxis is None
+
+    x = np.arange(3)
+    assert x.shape == (3,)
+    assert x[:, np.newaxis].shape == (3, 1)
+    assert x[:, np.newaxis].shape == x.reshape(3, -1).shape
+    assert x[np.newaxis].shape == (1, 3)
+
+
 def test_argmin_argmax():
     a = [[5, 4, 3], [10, 0, 5]]
 
