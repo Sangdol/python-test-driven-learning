ec57070036c65b331ca0122e1e076d29179cd08b
Sanghyun Lee <hammerha@gmail.com>
2019-12-14 22:29:31 +0100

    Add for if any list set comprehension test

--- test_list_comprehensions.py
+++ test_list_comprehensions.py
@@ -1,15 +1,21 @@
-import unittest
 
+def test_list_comprehension():
+    assert list(map(lambda x: x ** 2, range(3))) == [0, 1, 4]
+    assert [x ** 2 for x in range(3)] == [0, 1, 4]
 
-class ListComprehensionTest(unittest.TestCase):
+    comb = [(x, y) for x in [1, 2] for y in [2, 3] if x != y]
+    assert comb == [(1, 2), (1, 3), (2, 3)]
 
-    def test_list_comprehension(self):
-        self.assertEqual(list(map(lambda x: x ** 2, range(3))), [0, 1, 4])
-        self.assertEqual([x ** 2 for x in range(3)], [0, 1, 4])
+def test_for_if_any():
+    numbers = ['1', '11', '111', '2', '22', '222']
+    keys = ['1', '2']
 
-        comb = [(x, y) for x in [1, 2] for y in [2, 3] if x != y]
-        self.assertEqual(comb, [(1, 2), (1, 3), (2, 3)])
+    s = set()
+    for n in numbers:
+        for k in keys:
+            if n.count(k) == 3:
+                s.add(n)
 
+    ss = {n for n in numbers if any(n.count(k) == 3 for k in keys)}
 
-if __name__ == '__main__':
-    unittest.main()
+    assert s == ss
