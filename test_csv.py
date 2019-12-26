4c63bc07bb1610ae6a9ff0c4958672efe8bb813b
Sanghyun Lee <hammerha@gmail.com>
2019-12-26 16:18:07 +0100

    use next() instead of __next__()

--- test_csv.py
+++ test_csv.py
@@ -4,19 +4,19 @@ import csv
 def test_reader():
     with open('stub/test_csv.csv') as f:
         reader = csv.reader(f)
-        row = reader.__next__()
+        row = next(reader)
 
         assert type(row) == list
         assert row[0] == 'a'
 
-        row = reader.__next__()
+        row = next(reader)
         assert row[0] == '1'
 
 
 def test_dict_reader():
     with open('stub/test_csv.csv') as csvfile:
         reader = csv.DictReader(csvfile)
-        row = reader.__next__()
+        row = next(reader)
         a, b = row['a'], row['b']
 
         assert a == '1'
