c684c8f300b1176b5aafaa4bbc6e50e9d8070598
Sanghyun Lee <hammerha@gmail.com>
2020-02-17 22:04:40 +0100

    Add heapq tests

--- test_python_data_structures.py
+++ test_python_data_structures.py
@@ -1,4 +1,31 @@
 import pytest
+from heapq import heappush, heappop, heappushpop, heapreplace
+
+
+# https://docs.python.org/3/library/heapq.html
+def test_heapq():
+    """
+    This implementation uses arrays for which heap[k] <= heap[2*k+1] and heap[k] <= heap[2*k+2] for all k, counting elements from zero.
+
+    heapq, aka priority queue.
+    It's a min heap.
+    heapsort is not stable.
+    """
+    h = []
+    heappush(h, 3)
+    heappush(h, 2)
+    heappush(h, 1)
+
+    assert h == [1, 3, 2]
+    assert heappop(h) == 1
+
+    assert h == [2, 3]
+    assert heappushpop(h, 0) == 0
+
+    assert h == [2, 3]
+    assert heapreplace(h, -1) == 2
+
+    assert h == [-1, 3]
 
 
 def test_difference_set():
