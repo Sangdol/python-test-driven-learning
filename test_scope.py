bb2ddbf693abaafeb059abff6668dc9a89ada82d
Sanghyun Lee <hammerha@gmail.com>
2020-02-29 15:46:29 +0100

    Add closure tests

--- test_scope.py
+++ test_scope.py
@@ -1,3 +1,31 @@
+def test_closure():
+    """Any function that has free variables that are not global vairables is a closure.
+
+    free variable - a variable used in a function, which is neither a formal parameter
+        to the function nor defined in the functions body
+
+    Refer to https://stackoverflow.com/questions/4020419/why-arent-python-nested-functions-called-closures
+    """
+    def counter():
+        count = 0
+
+        def increase():
+            nonlocal count
+            count += 1
+            return count
+
+        return increase
+
+    inc = counter()
+    assert inc() == 1
+    assert inc() == 2
+
+    # about cell and __closure__ https://docs.python.org/3/reference/datamodel.html
+    assert not counter.__closure__  # it has the attribute but empty.
+    assert inc.__closure__  # tuples of cell
+    assert inc.__closure__[0].cell_contents == 2
+
+
 global_abc = 1
 
 
