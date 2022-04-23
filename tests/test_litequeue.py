f30f9e7855b5b4569fe7c34f69e8c95652e3c9cc
Sanghyun Lee <hammerha@gmail.com>
2022-04-23 17:49:25 +0200

    Add litequeue tests

---
+++ tests/test_litequeue.py
@@ -0,0 +1,37 @@
+"""litequeue tests
+
+* Docs      https://litements.exampl.io/queue/
+* Github    https://github.com/litements/litequeue
+"""
+
+from litequeue import SQLQueue
+
+
+def test_basic():
+    q = SQLQueue(':memory:')
+
+    assert q.put('a') == 1
+    assert q.put('b') == 2
+
+    """
+    e.g,
+    {
+        'message': 'a',
+        'message_id': '1e49f8676615a8c900094a638c5edfde',
+        'status': 1,
+        'in_time': 1650726882,
+        'lock_time': 1650726925,
+        'done_time': None
+    }
+    """
+    task = q.pop()
+
+    assert task['message'] == 'a'
+    assert task['status'] == 1
+
+    q.done(task['message_id'])
+
+    # The status of task changes to 2 in the database
+    # but it doesn't change the task itself.
+    assert task['status'] == 1
+
