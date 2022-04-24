4ceaa12726bf0b029949a81d7ff08c9e4ce6d372
Sanghyun Lee <hammerha@gmail.com>
2022-04-24 14:37:42 +0200

    Add test requests with headers

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -14,3 +14,19 @@ def test_requests():
     assert response.url == 'https://httpbin.org/get?param1=value1&param2=value2'
     assert response.json()['args'] == params
     assert response.json()['headers']['Host'] == 'httpbin.org'
+
+
+def test_requests_with_headers():
+    url = 'https://httpbin.org/get'
+
+    # For some reason, the server capitalize the first character of header names
+    # so the test will fail if the header name is not capitalized.
+    headers = {'Param1': 'value1', 'Param2': 'value2'}
+    response = requests.get(url, headers=headers)
+
+    # print(response.json())
+    assert response.status_code == 200
+    assert response.url == 'https://httpbin.org/get'
+    assert response.json()['headers'].items() >= headers.items()
+    assert response.json()['headers']['Host'] == 'httpbin.org'
+
