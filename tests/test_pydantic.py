c1ddc186bfc067f54a3fae6f33510bf7af46331e
Sanghyun Lee <hammerha@gmail.com>
2022-05-03 18:15:57 +0200

    Add validator test

--- tests/test_pydantic.py
+++ tests/test_pydantic.py
@@ -3,28 +3,29 @@ https://pydantic-docs.helpmanual.io/
 """
 from datetime import datetime
 from typing import List, Optional
-from pydantic import BaseModel
+from decimal import Decimal
+
+from pydantic import BaseModel, validator
 from pydantic.dataclasses import dataclass
 
 
 def test_basic():
     class User(BaseModel):
         id: int
-        name = 'John Doe'
+        name = "John Doe"
         signup_ts: Optional[datetime] = None
         friends: List[int] = []
 
-
     external_data = {
-        'id': '123',
-        'signup_ts': '2019-06-01 12:22',
-        'friends': [1, 2, '3'],
+        "id": "123",
+        "signup_ts": "2019-06-01 12:22",
+        "friends": [1, 2, "3"],
     }
 
     user = User(**external_data)
 
     assert user.id == 123
-    assert user.name == 'John Doe'
+    assert user.name == "John Doe"
     assert user.signup_ts == datetime(2019, 6, 1, 12, 22)
     assert user.friends == [1, 2, 3]
 
@@ -32,7 +33,7 @@ def test_basic():
 def test_getitem():
     class User(BaseModel):
         id: int
-        name = 'John Doe'
+        name = "John Doe"
         signup_ts: Optional[datetime] = None
         friends: List[int] = []
 
@@ -41,14 +42,28 @@ def test_getitem():
         def __getitem__(self, item):
             return getattr(self, item)
 
-
     external_data = {
-        'id': '123',
-        'signup_ts': '2019-06-01 12:22',
-        'friends': [1, 2, '3'],
+        "id": "123",
+        "signup_ts": "2019-06-01 12:22",
+        "friends": [1, 2, "3"],
     }
 
     user = User(**external_data)
 
     assert user.id == 123
-    assert user['id'] == 123
+    assert user["id"] == 123
+
+
+def test_validator():
+    class User(BaseModel):
+        age: str
+
+        @validator("age")
+        def validate_age(cls, v):
+            if int(v) < 0:
+                raise ValueError("Age must be positive")
+            return int(v)
+
+    user = User(age="1")
+    assert user.age == 1
+    assert type(user.age) == int
