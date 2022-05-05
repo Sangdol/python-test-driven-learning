3720092f192e4d0fa63d0f402ecbded1ae900b0d
Sanghyun Lee <hammerha@gmail.com>
2022-05-05 20:52:02 +0200

    Add KeysView type test

--- tests/test_type_annotations.py
+++ tests/test_type_annotations.py
@@ -4,6 +4,7 @@ Some code is from the book Robust Python.
 from dataclasses import dataclass
 from typing import TypeVar, List, Optional, Union, Literal, Set, Tuple, Final, TypedDict
 from decimal import Decimal
+from collections.abc import KeysView
 
 
 def test_method():
@@ -20,20 +21,20 @@ def test_method():
 
 
 def test_type_var():
-    T = TypeVar('T')
+    T = TypeVar("T")
 
     # Generics - T has to be the same type.
     def half(l: List[T]) -> List[T]:
-        return l[:len(l) // 2]
+        return l[: len(l) // 2]
 
     assert half([1, 2]) == [1]
 
 
 def test_optional():
-    maybe_a_string: Optional[str] = 'abc'
+    maybe_a_string: Optional[str] = "abc"
     maybe_an_int: Optional[int] = None
 
-    assert maybe_a_string == 'abc'
+    assert maybe_a_string == "abc"
     assert maybe_an_int is None
 
     # This can return None thanks to Optional.
@@ -46,44 +47,42 @@ def test_optional():
 def test_union():
     def union(flag: bool) -> Union[str, int]:
         if flag:
-            return '1'
+            return "1"
         else:
             return 1
 
-    assert union(True) == '1'
+    assert union(True) == "1"
 
 
 def test_literal():
-
     @dataclass
     class Error:
-        error_code: Literal[1,2,3]
+        error_code: Literal[1, 2, 3]
 
     @dataclass
     class Snack:
-        name: Literal['Pretzel', 'Hotdog']
-        condiments: Set[Literal['Mustard', 'Ketchup']]
+        name: Literal["Pretzel", "Hotdog"]
+        condiments: Set[Literal["Mustard", "Ketchup"]]
 
     # 0 is not possible
     assert Error(1).error_code == 1
 
     # These are okay with mypy but not okay with IntelliJ type checker.
-    assert Snack('Pretzel', set(['Mustard', 'Ketchup']))
-    assert Snack('Pretzel', {'Mustard', 'Ketchup'})
+    assert Snack("Pretzel", set(["Mustard", "Ketchup"]))
+    assert Snack("Pretzel", {"Mustard", "Ketchup"})
 
 
 def test_func():
-
     def tuple() -> Tuple[str, int]:
-        return 'a', 1
+        return "a", 1
 
-    assert tuple() == ('a', 1)
+    assert tuple() == ("a", 1)
 
 
 def test_final():
-    NAME: Final = 'Sang'
+    NAME: Final = "Sang"
 
-    assert NAME == 'Sang'
+    assert NAME == "Sang"
 
 
 def test_typed_dict():
@@ -91,7 +90,7 @@ def test_typed_dict():
         name: str
         age: int
 
-    assert Person(name='Sang', age=30) == {'name': 'Sang', 'age': 30}
+    assert Person(name="Sang", age=30) == {"name": "Sang", "age": 30}
 
 
 def test_decimal_sum():
@@ -102,3 +101,11 @@ def test_decimal_sum():
 
     assert return_decimal() == Decimal(3)
 
+
+def test_keys_view():
+    d = {1: "a", 2: "b"}
+
+    def keys_view() -> KeysView[int]:
+        return d.keys()
+
+    assert list(keys_view()) == [1, 2]
