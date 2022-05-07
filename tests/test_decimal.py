3623cd263213e9a0db2a0c2472c05c421c49dd9e
Sanghyun Lee <hammerha@gmail.com>
2022-05-07 19:18:37 +0200

    Update Decimal comparison tests

--- tests/test_decimal.py
+++ tests/test_decimal.py
@@ -3,38 +3,35 @@ import math
 
 
 def test_comparison():
-    assert Decimal("1.0") == Decimal("1.0")
-    assert Decimal("1.0") == Decimal(1.0)
+    assert Decimal('1.0') == Decimal('1.0')
 
-    # Be careful 1
-    assert Decimal(1.1) == 1.1
-    assert Decimal(1.1) != Decimal("1.1")
+    # Be careful 1. don't pass a float to Decimal
+    assert Decimal(1.1) == 1.1  # the 1.1 arugment is already flaot
+    assert Decimal(1.1) != Decimal('1.1')  # so this is not equal
     assert Decimal(1.1) == Decimal(
-        "1.100000000000000088817841970012523233890533447265625"
+        '1.100000000000000088817841970012523233890533447265625'
     )
-
-    # Be careful 2
     assert Decimal(0.3) == 0.3
     assert Decimal(0.1 + 0.1 + 0.1) != 0.3
     assert Decimal(0.1 + 0.1 + 0.1) != Decimal(0.3)
 
-    # Compare doesn't help
-    # 1: bigger, -1: smaller, 0: equal
-    assert Decimal(1.1).compare(Decimal("1.1")) == 1
+    # This sometimes works which makes things more confusing
+    assert Decimal('1.0') == Decimal(1.0)
 
-    # Be careful 3
-    assert Decimal("0.30") > 0.3
+    # Be careful 2. don't compare a float to Decimal
+    assert Decimal('0.30') > 0.3
 
     # Rounding doesn't help
-    assert round(Decimal("0.30"), 2) > 0.3
+    assert Decimal('0.30') > round(0.3, 2)
 
     # Rounding helps in some cases
-    assert Decimal(0.3) - Decimal("0.3") != 0
-    assert round(Decimal(0.3) - Decimal("0.3"), 2) == 0
+    assert Decimal(0.3) - Decimal('0.3') != 0
+    assert round(Decimal(0.3) - Decimal('0.3'), 2) == 0
 
     # 0 always works
-    assert Decimal("0.0") == 0
-    assert Decimal("0.0") == Decimal(0)
+    assert Decimal('0.0') == 0
+    assert Decimal('0.0') == Decimal(0)
 
-    # Workaround
-    assert math.isclose(Decimal("0.30"), 0.3)
+    # Workaround / right way
+    assert math.isclose(Decimal('0.30'), 0.3)
+    assert Decimal('0.1') + Decimal('0.1') + Decimal('0.1') == Decimal('0.3')
