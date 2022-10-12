0921381fee9a8fb69e0c6e189e823e343bf0b40f
Sanghyun Lee <hammerha@gmail.com>
2022-10-12 16:06:32 +0200

    Add get_type_hints tests

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,3 +1,6 @@
+from typing import Annotated, NamedTuple, get_type_hints
+
+
 # No way to get a full name
 # https://stackoverflow.com/questions/15165101/how-to-get-full-type-name-in-python
 def test_module_and_name():
@@ -12,3 +15,13 @@ def test_type_checking():
 
     # isinstance is working for type inheritance
     assert isinstance({}, dict)
+
+
+# https://docs.python.org/3/library/typing.html#typing.get_type_hints
+def test_get_type_hints():
+    class Student(NamedTuple):
+        name: Annotated[str, 'some marker']
+
+    assert get_type_hints(Student) == {'name': str}
+    assert get_type_hints(Student, include_extras=False) == {'name': str}
+    assert get_type_hints(Student, include_extras=True) == {'name': Annotated[str, 'some marker']}
