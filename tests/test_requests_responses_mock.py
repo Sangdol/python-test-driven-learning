0e03784c953c62ded4abd2be8d67076068001780
Sanghyun Lee <hammerha@gmail.com>
2022-10-18 16:10:26 +0200

    Revert "Remove unnecessary @responses.activate annotations"
    This reverts commit 75efe92e89339e21bb79d3f342c835cef8d038cd.

--- tests/test_requests_responses_mock.py
+++ tests/test_requests_responses_mock.py
@@ -8,18 +8,32 @@ import re
 from responses import matchers
 
 
+@responses.activate
 def test_responses():
     url = 'https://test.com'
-    responses.add(responses.POST, url, json={'a': 1}, status=200)
+    responses.add(
+        responses.POST,
+        url,
+        json={'a': 1}, status=200)
 
     assert requests.post(url).content == b'{"a": 1}'
 
 
+@responses.activate
 def test_multiple_responses():
     url = 'https://test.com'
-    responses.add(responses.POST, url, json={'a': 1}, status=200)
-    responses.add(responses.POST, url, json={'a': 1}, status=200)
-    responses.add(responses.POST, url, json={'a': 2}, status=200)
+    responses.add(
+        responses.POST,
+        url,
+        json={'a': 1}, status=200)
+    responses.add(
+        responses.POST,
+        url,
+        json={'a': 1}, status=200)
+    responses.add(
+        responses.POST,
+        url,
+        json={'a': 2}, status=200)
 
     assert requests.post(url).content == b'{"a": 1}'
     assert requests.post(url).content == b'{"a": 1}'
@@ -27,19 +41,21 @@ def test_multiple_responses():
     assert requests.post(url).content == b'{"a": 2}'
 
 
+@responses.activate
 def test_responses_url_pattern():
     url = re.compile('https://test.com/')
-    responses.add(responses.POST, url, json={'a': 1}, status=200)
+    responses.add(
+        responses.POST,
+        url,
+        json={'a': 1}, status=200)
 
-    assert (
-        requests.post(
-            'https://test.com/abc?a=1&b=2',
-            headers={'Content-Type': 'application/json; charset=UTF-8'},
-        ).content
-        == b'{"a": 1}'
-    )
+    assert requests.post(
+        'https://test.com/abc?a=1&b=2',
+        headers={'Content-Type': 'application/json; charset=UTF-8'},
+    ).content == b'{"a": 1}'
 
 
+@responses.activate
 def test_responses_matchers_header():
     url = 'https://test.com/'
     headers1 = {'header1': 'value1'}
@@ -49,30 +65,21 @@ def test_responses_matchers_header():
         responses.POST,
         url,
         match=[matchers.header_matcher(headers1)],
-        json={'a': 1},
-        status=200,
-    )
+        json={'a': 1}, status=200)
 
     responses.add(
         responses.POST,
         url,
         match=[matchers.header_matcher(headers2)],
-        json={'a': 2},
-        status=200,
-    )
+        json={'a': 2}, status=200)
 
-    assert (
-        requests.post(
-            'https://test.com/',
-            headers=headers1,
-        ).content
-        == b'{"a": 1}'
-    )
+    assert requests.post(
+        'https://test.com/',
+        headers=headers1,
+    ).content == b'{"a": 1}'
 
-    assert (
-        requests.post(
-            'https://test.com/',
-            headers=headers2,
-        ).content
-        == b'{"a": 2}'
-    )
+    assert requests.post(
+        'https://test.com/',
+        headers=headers2,
+    ).content == b'{"a": 2}'
+
